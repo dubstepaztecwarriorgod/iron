ae796a15499fa5ebcc43e2b66d13122cb2848877
largenumberhere <largeNumbergoeshere@gmail.com>
2024-11-17 19:01:56 +1100

    felt cute, might revert later

--- Makefile
+++ Makefile
@@ -2,4 +2,4 @@ qemu:
 	cargo install cargo-xbuild # bootimage dependency
 	cargo install bootimage
 	cd iron; cargo +nightly bootimage
-	qemu-system-x86_64 -drive format=raw,file=.\iron\target\x86_64-unknown-none\debug\bootimage-iron.bin
+	qemu-system-x86_64 -drive format=raw,file=./iron/target/iron/debug/bootimage-iron.bin
--- iron/src/main.rs
+++ iron/src/main.rs
@@ -3,23 +3,137 @@
 
 // pub use core as core;
 
+
+use core::fmt::{Arguments, Debug, Pointer, Write};
 use core::panic::PanicInfo;
-
-
+use bootloader::BootInfo;
+use bootloader::entry_point;
+use ufmt::uWrite;
+use ufmt::uwrite;
 #[panic_handler]
-fn panic(_: &PanicInfo) -> ! {
-    loop {}
+fn panic(panic_info: &PanicInfo) -> ! {
+    let mut vga = Vga::new();
+    vga.clear();
+    let mut formatter = ufmt::Formatter::new(&mut vga);
+    let _ = formatter.write_str("Kernel Panic: ");
+
+    let message = panic_info.message();
+    if let Some(m) = message.as_str() {
+        uwrite!(&mut formatter, "'{}'", m).unwrap();
+    } else {
+        let _ = formatter.write_str("unknown panic message");
+    }
+
+    if let Some(location) = panic_info.location() {
+        let file = location.file();
+        let line = location.line();
+        let column = location.column();
+        uwrite!(&mut formatter,"{}:{} {}", file, line, column);
+    } else {
+        uwrite!(&mut formatter, "Unknown panic location");
+    }
+
+    halt();
 }
 
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
-    loop {
 
 
+
+struct Vga {
+    buffer_index: usize,
+}
+
+// A simple wrapping vga buffer writer
+impl Vga {
+    const BUFFER_HEIGHT: usize = 25;
+    const BUFFER_WIDTH: usize = 80;
+    fn new() -> Self {
+        Vga { buffer_index: 0 }
+    }
+
+    fn push_char(&mut self, c: char) {
+        if c == '\n' {
+            let row =  self.buffer_index / Self::BUFFER_WIDTH as usize;
+            let row = row + 1;
+            self.buffer_index = row * Self::BUFFER_WIDTH;
+            return;
+        }
+
+        let max_chars = (Self::BUFFER_HEIGHT * Self::BUFFER_WIDTH);
+        if self.buffer_index >= max_chars {
+            self.buffer_index = 0;
+        }
+
+        let vga_start = (0xb8000 as *mut u8);
+        let color_white = 15;
+        unsafe {
+            vga_start.offset((self.buffer_index*2) as isize).write_volatile(c as u8);
+            vga_start.offset((self.buffer_index*2+1) as isize).write_volatile(color_white);
+        }
+
+        self.buffer_index += 1;
+    }
+
+    fn clear(&mut self) {
+        for _ in 0..Self::BUFFER_HEIGHT* Self::BUFFER_WIDTH {
+            self.push_char(' ');
+        }
+    }
+
+    fn flood(&mut self, c: char) {
+        for _ in 0..Self::BUFFER_WIDTH * Self::BUFFER_HEIGHT {
+            self.push_char(c);
+        }
+    }
+
+    fn push_str(&mut self, string: &str) {
+        for c in string.chars() {
+            self.push_char(c);
+        }
     }
 }
 
-// #[no_mangle]
-// fn entry_32_bit() -> ! {
-//     loop {}
-// }
+impl uWrite for Vga {
+    type Error = ();
+
+
+    fn write_str(&mut self, s: &str) -> Result<(), Self::Error> {
+        self.push_str(s);
+        return Ok(());
+    }
+
+    fn write_char(&mut self, c: char) -> Result<(), Self::Error> {
+        self.push_char(c);
+        Ok(())
+    }
+}
+
+// burn some cpu cycles. Around a second in QEMU
+fn delay(count: usize) {
+    for i in 0..count* 7_000_000 {}
+}
+
+fn kernel() {
+    let mut vga = Vga::new();
+    vga.clear();
+    delay(1);
+    vga.flood('a');
+    delay(1);
+    vga.flood('b');
+    delay(1);
+    vga.clear();
+
+    panic!("meow!");
+}
+
+entry_point!(kernel_start);
+
+#[no_mangle]
+fn kernel_start(a: &BootInfo) -> ! {
+    kernel();
+    halt();
+}
+
+fn halt() -> ! {
+    loop {}
+}
